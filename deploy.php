9f5a28bfdb3ee26aa6c7e60656fe77ac74aa2eac
Maksim Kuznetsov <mk@4xxi.com>
2017-01-12 19:15:14 +0300

    [Deploy] Added deployer

---
+++ deploy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Deployer;
+
+require 'vendor/deployer/deployer/recipe/symfony.php';
+
+serverList('app/config/deployer/servers.yml');
+
+set('bin_dir', 'bin');
+set('default_stage', 'staging');
+
+set('repository', 'git@github.com:4xxi/keys.git');
+set('keep_releases', 4);
+
+set('http_user', 'www-data');
+
+set('shared_dirs', ['var/logs',]);
+set('shared_files', ['app/config/parameters.yml']);
+set('writable_dirs', ['var/cache', 'var/logs', 'var/sessions']);
+set('assets', ['web/css', 'web/images', 'web/js']);
+
+task('reload:php-fpm', function () {
+    run('sudo service php7.1-fpm reload');
+});
+
+//after('deploy:symlink', 'database:migrate');
+after('deploy', 'reload:php-fpm');
+after('rollback', 'reload:php-fpm');
